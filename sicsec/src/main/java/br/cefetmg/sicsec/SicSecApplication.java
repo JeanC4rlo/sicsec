3fafecd5b7b22a34c036185175d986da1c69eb49
Davi G S Freitas <daviguifrei@gmail.com>
2025-10-20 09:38:00 -0300

    criando repositório

---
+++ sicsec/src/main/java/br/cefetmg/sicsec/SicSecApplication.java
@@ -0,0 +1,13 @@
+package br.cefetmg.sicsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicSecApplication.class, args);
+	}
+
+}
---
+++ sicsec/src/test/java/br/cefetmg/sicsec/SicSecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.cefetmg.sicsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
